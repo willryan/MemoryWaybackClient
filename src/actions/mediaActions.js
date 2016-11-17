7ef2c6da43195929243ababdfbab629fd46f592e
Will Pleasant-Ryan <will.ryan@atomicobject.com>
2016-11-17 15:47:45 -0500

    fix params

--- src/actions/mediaActions.js
+++ src/actions/mediaActions.js
@@ -1,15 +1,17 @@
 import axios from "axios";
+import queryString from "query-string";
 
 export function fetchMedia(query) {
   const types = query.types.filter(t => t.include).map(t => t.name);
-  const fromDate = new Date(query.fromDate);
-  const toDate = new Date(query.toDate);
+  const from = new Date(query.fromDate).toISOString();
+  const to = new Date(query.toDate).toISOString();
+  const params = queryString.stringify({from, to, types});
   return dispatch => {
     dispatch({
       type: "FETCH_MEDIA",
       payload: {}
     })
-    axios.get("/api/media-query")
+    axios.get(`/api/media-query?${params}`)
       .then(resp => {
         dispatch({
           type: "FETCH_MEDIA_FULFILLED",
