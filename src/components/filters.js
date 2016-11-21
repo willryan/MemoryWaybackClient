83ac2bed752dd7b550038b91ecfdbf41c483feb6
Will Pleasant-Ryan <will.ryan@atomicobject.com>
2016-11-20 19:52:33 -0500

    a few more styles

--- src/components/filters.js
+++ src/components/filters.js
@@ -6,15 +6,15 @@ export default class Filters extends Component {
   }
   render() {
     const { filter } = this.props
-    const mappedTypes = filter.map(m => <li key={m.name}>
+    const mappedTypes = filter.map(m => <span key={m.name}>
       <input type="checkbox"
              checked={m.include}
              onChange={this.onChange.bind(this,m.name)}>
       </input>
       {m.name}
-    </li>)
+    </span>)
     return (
-      <ul>{mappedTypes}</ul>
+      <div>{mappedTypes}</div>
     );
   }
 }
